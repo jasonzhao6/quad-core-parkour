3914a75b09a5b59b25ed495885cd8e0dd7b46cae
Jason Zhao <jasonzhao6@gmail.com>
2018-09-30 19:20:33 -0700

    Add directional methods for getting neighboring cores

--- vanilla/js/models/Core.js
+++ vanilla/js/models/Core.js
@@ -11,4 +11,44 @@ export default class Core {
     this.accumulator = Core.DEFAULT_VALUE;
     this.backup = Core.DEFAULT_VALUE;
   }
+
+  //
+  // Neighbors
+  //
+
+  up() {
+    const neighborI = this.i - 1;
+    const neighborJ = this.j;
+
+    if (neighborI === -1) return null;
+
+    return this.matrix.get(neighborI, neighborJ);
+  }
+
+  down() {
+    const neighborI = this.i + 1;
+    const neighborJ = this.j;
+
+    if (neighborI === this.matrix.rowCount) return null;
+
+    return this.matrix.get(neighborI, neighborJ);
+  }
+
+  left() {
+    const neighborI = this.i;
+    const neighborJ = this.j - 1;
+
+    if (neighborJ === -1) return null;
+
+    return this.matrix.get(neighborI, neighborJ);
+  }
+
+  right() {
+    const neighborI = this.i;
+    const neighborJ = this.j + 1;
+
+    if (neighborJ === this.matrix.columnCount) return null;
+
+    return this.matrix.get(neighborI, neighborJ);
+  }
 }
--- vanilla/js/models/__tests__/CoreTest.js
+++ vanilla/js/models/__tests__/CoreTest.js
@@ -1,10 +1,11 @@
 import Core from '../Core.js';
+import Matrix from '../Matrix.js';
 
 export default class CoreTest {
   static enqueue(_) {
     _.Class(Core, () => {
       _.method('#constructor', () => {
-        _.context('When creating a core without matrix params', () => {
+        _.context('When creating a core without matrix args', () => {
           const subject = new Core();
 
           _.assert(
@@ -17,7 +18,7 @@ export default class CoreTest {
           );
         });
 
-        _.context('When creating a core with matrix params', () => {
+        _.context('When creating a core with matrix args', () => {
           const [i, j, matrix] = [1, 2, {}];
           const subject = new Core({ i, j, matrix });
 
@@ -35,13 +36,95 @@ export default class CoreTest {
           const subject = new Core();
 
           _.assert(
-            'It initializes accumulator to default value',
+            'It initializes `accumulator` to default value',
             () => subject.accumulator === Core.DEFAULT_VALUE,
           );
 
           _.assert(
-            'It initializes backup to default value',
+            'It initializes `backup` to default value',
             () => subject.backup === Core.DEFAULT_VALUE,
+          );
+        });
+      });
+
+      const twoByTwo = new Matrix({ rowCount: 2, columnCount: 2, Class: Core });
+
+      _.method('#up', () => {
+        _.context('When core is at [0][0] inside a 2x2 matrix', () => {
+          const subject = twoByTwo.get(0, 0);
+
+          _.assert(
+            'It returns null',
+            () => subject.up() === null,
+          );
+        });
+
+        _.context('When core is at [1][0] inside a 2x2 matrix', () => {
+          const subject = twoByTwo.get(1, 0);
+
+          _.assert(
+            'It returns core above',
+            () => subject.up() === twoByTwo.get(0, 0),
+          );
+        });
+      });
+
+      _.method('#down', () => {
+        _.context('When core is at [0][0] inside a 2x2 matrix', () => {
+          const subject = twoByTwo.get(0, 0);
+
+          _.assert(
+            'It returns core below',
+            () => subject.down() === twoByTwo.get(1, 0),
+          );
+        });
+
+        _.context('When core is at [1][0] inside a 2x2 matrix', () => {
+          const subject = twoByTwo.get(1, 0);
+
+          _.assert(
+            'It returns null',
+            () => subject.down() === null,
+          );
+        });
+      });
+
+      _.method('#left', () => {
+        _.context('When core is at [0][0] inside a 2x2 matrix', () => {
+          const subject = twoByTwo.get(0, 0);
+
+          _.assert(
+            'It returns null',
+            () => subject.left() === null,
+          );
+        });
+
+        _.context('When core is at [0][1] inside a 2x2 matrix', () => {
+          const subject = twoByTwo.get(0, 1);
+
+          _.assert(
+            'It returns core to the left',
+            () => subject.left() === twoByTwo.get(0, 0),
+          );
+        });
+      });
+
+      _.method('#right', () => {
+        _.context('When core is at [0][0] inside a 2x2 matrix', () => {
+          const subject = twoByTwo.get(0, 0);
+
+          _.assert(
+            'It returns core to the right',
+            () => subject.right() === twoByTwo.get(0, 1),
+          );
+        });
+
+        _.context('When core is at [0][1] inside a 2x2 matrix', () => {
+          const subject = twoByTwo.get(0, 1);
+
+          _.assert(
+            'It returns null',
+            () => subject.right() === null,
           );
         });
       });
