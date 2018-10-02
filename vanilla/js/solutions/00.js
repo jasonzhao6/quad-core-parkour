dc3128e6cee67018009c90b1263b24c5d2bd0873
Jason Zhao <jasonzhao6@gmail.com>
2018-10-01 22:43:24 -0700

    Add solution to level 00

---
+++ vanilla/js/solutions/00.js
@@ -0,0 +1,15 @@
+export const oo = [
+  'mov up down',
+];
+
+export const oi = [
+  'mov up down',
+];
+
+export const io = [
+  'mov up down',
+];
+
+export const ii = [
+  'mov up down',
+];
--- vanilla/main.js
+++ vanilla/main.js
@@ -2,6 +2,11 @@ import './__FileWatcherClient__.js';
 import './__Tests__.js';
 
 import { input, output } from './js/levels/00.js';
+import { oo, oi, io, ii } from './js/solutions/00.js';
 
-console.log(JSON.stringify(input));
-console.log(JSON.stringify(output));
+console.log('input:', JSON.stringify(input));
+console.log('output:', JSON.stringify(output));
+console.log('oo:', JSON.stringify(oo));
+console.log('oi:', JSON.stringify(oi));
+console.log('io:', JSON.stringify(io));
+console.log('ii:', JSON.stringify(ii));
